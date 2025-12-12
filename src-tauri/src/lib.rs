153e0da4f4244565fec6681d027384e345da03cd
Vrtgs <vrtgs@vrtgs.xyz>
2025-12-12 19:09:32 +0300

    stable app

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,6 @@
-use std::sync::{LazyLock, OnceLock};
+use std::sync::{LazyLock, Once, OnceLock};
 use reqwest::{Client, Url};
 use serde::{Deserialize, Serialize};
-use tauri_plugin_opener::OpenerExt;
 use tokio::sync::{oneshot, watch, OnceCell};
 
 #[derive(Copy, Clone, Deserialize, Serialize)]
@@ -22,7 +21,7 @@ pub struct RequestedAutofillFields {
     id_image: bool,
 }
 
-#[derive(Clone, Default, Deserialize, Serialize)]
+#[derive(Clone, Default, Deserialize, Serialize, Eq, PartialEq)]
 pub struct AutofillData {
     name: Option<[String; 2]>,
     email: Option<String>,
@@ -33,6 +32,19 @@ pub struct AutofillData {
     /// stored as a base64 image
     license: Option<String>,
     id_image: Option<String>,
+}
+
+#[derive(Clone, Default, Deserialize, Serialize, Eq, PartialEq)]
+pub struct AutofillDataRef<'a> {
+    name: Option<[&'a str; 2]>,
+    email: Option<&'a str>,
+    phone_number: Option<&'a str>,
+    id: Option<&'a str>,
+    /// stored as a base64 image
+    profile_picture: Option<&'a str>,
+    /// stored as a base64 image
+    license: Option<&'a str>,
+    id_image: Option<&'a str>,
 }
 
 static BASE_URL: LazyLock<Url> = LazyLock::new(|| {
@@ -56,7 +68,7 @@ fn get_client() -> reqwest::Result<&'static Client> {
 
 const USER_DATA_PATH: &str = "./user-data.json";
 
-static USER_DATA_CHANNEL: OnceCell<(watch::Sender<AutofillData>, watch::Receiver<AutofillData>)> = OnceCell::new();
+static USER_DATA_CHANNEL: OnceCell<(watch::Sender<AutofillData>, watch::Receiver<AutofillData>)> = OnceCell::const_new();
 
 async fn get_user_data_channel() -> &'static (watch::Sender<AutofillData>, watch::Receiver<AutofillData>) {
     USER_DATA_CHANNEL
@@ -64,9 +76,9 @@ async fn get_user_data_channel() -> &'static (watch::Sender<AutofillData>, watch
         .expect("failed to initalize at the start of teh program")
 }
 
-async fn load_data() -> watch::Ref<AutofillData> {
+async fn with_load_data<T>(fun: impl FnOnce(&AutofillData) -> T) -> T {
     let (_rx, tx) = get_user_data_channel().await;
-    tx.borrow()
+    fun(&tx.borrow())
 }
 
 async fn store_data(user_data: AutofillData) {
@@ -75,56 +87,168 @@ async fn store_data(user_data: AutofillData) {
 }
 
 #[tauri::command]
-pub async fn fetch_request_info(code: &str) -> anyhow::Result<RequestedAutofillFields> {
-    let result = get_client()?.get(request_url(code)?)
-        .await?
-        .json()?;
-
-    Ok(result)
+async fn load_data_from_store() -> AutofillData {
+    with_load_data(AutofillData::clone).await
 }
 
 #[tauri::command]
-pub async fn confirm_request(code: &str) -> anyhow::Result<()> {
-    let ref_lock = load_data();
-    let request = get_client()?
-        .post(request_url(code)?)
-        .json(&ref_lock);
-
-    drop(ref_lock);
-
-    request
-        .send()
-        .await?
-        .error_for_status()?;
-
-    Ok(())
+async fn store_data_to_store(data: AutofillData) {
+    eprintln!("submitted data store request!");
+    store_data(data).await;
 }
 
-#[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
-    let (send_channel, rcv_channel) = oneshot::channel();
-    let (started_init, wait_for_start) = oneshot::channel();
+#[tauri::command]
+async fn fetch_request_info(code: &str) -> Result<RequestedAutofillFields, String> {
+    async fn fetch_request_info(code: &str) -> anyhow::Result<RequestedAutofillFields> {
+        let result = get_client()?
+            .get(request_url(code)?)
+            .send()
+            .await?
+            .json()
+            .await?;
 
-    tauri::async_runtime::spawn(USER_DATA_CHANNEL.get_or_init(async || {
-        started_init.send(()).unwrap();
-        rcv_channel.await.failed_to_initialize()
-    }));
+        Ok(result)
+    }
+
+    fetch_request_info(code).await.map_err(|err| err.to_string())
+}
+
+#[tauri::command]
+async fn confirm_request(code: &str, filter: RequestedAutofillFields) -> Result<(), String> {
+    async fn confirm_request(code: &str, filter: RequestedAutofillFields) -> anyhow::Result<()> {
+        let client = get_client()?;
+        let url = request_url(code)?;
+        let request = with_load_data(|autofill_data| {
+            let name = filter
+                .name
+                .then(|| {
+                    autofill_data
+                        .name
+                        .as_ref()
+                        .map(|names| names.each_ref().map(String::as_str))
+                })
+                .flatten();
+
+            let json = AutofillDataRef {
+                name,
+                email: filter.email.then_some(autofill_data.email.as_deref()).flatten(),
+                phone_number: filter.phone_number.then_some(autofill_data.phone_number.as_deref()).flatten(),
+                id: filter.id.then_some(autofill_data.id.as_deref()).flatten(),
+                profile_picture: filter.profile_picture.then_some(autofill_data.profile_picture.as_deref()).flatten(),
+                license: filter.license.then_some(autofill_data.license.as_deref()).flatten(),
+                id_image: filter.id_image.then_some(autofill_data.id_image.as_deref()).flatten(),
+            };
+
+            client.post(url).json(&json)
+        }).await;
+
+        request
+            .send()
+            .await?
+            .error_for_status()?;
+
+        Ok(())
+    }
+
+    confirm_request(code, filter).await.map_err(|err| err.to_string())
+}
+
+
+fn setup_once(
+    _app: &mut tauri::App,
+    wait_for_start: oneshot::Receiver<()>,
+    send_channel: oneshot::Sender<(watch::Sender<AutofillData>, watch::Receiver<AutofillData>)>
+) {
+    #[cfg(target_os = "android")]
+    {
+        use tauri::Manager;
+
+        let app = _app;
+        std::env::set_current_dir(app.path().app_data_dir().unwrap())
+            .unwrap();
+    }
+
+    #[cfg(target_vendor = "apple")]
+    {
+        std::env::set_current_dir(
+            "/Users/vrtgs/Coding/Rust/android-apps/absher-zt/dist"
+        ).unwrap();
+    }
+
+
     tauri::async_runtime::block_on(wait_for_start).unwrap();
 
     tauri::async_runtime::spawn(async move {
         let user_data = tokio::fs::read_to_string(USER_DATA_PATH)
             .await
-            .and_then(|str| serde_json::from_str(&str))
-            .unwrap_or_default();
+            .map_err(anyhow::Error::new)
+            .and_then(|str| serde_json::from_str(&str).map_err(anyhow::Error::new))
+            .unwrap_or_else(|err| {
+                eprintln!("failed to get user information: {err}");
+                AutofillData {
+                    name: Some(["foo", "bar"].map(str::to_string)),
+                    email: Some("mail@mail.com".to_string()),
+                    phone_number: Some("0567814779".to_owned()),
+                    id: Some("1122334466".to_owned()),
+                    ..AutofillData::default()
+                }
+            });
 
-        let (tx, rx) = watch::channel(user_data);
+        let (tx, mut rx) = watch::channel::<AutofillData>(user_data.clone());
+        send_channel.send((tx, rx.clone()))
+            .ok()
+            .expect("failed to send");
 
-        send_channel.send((tx, rx))
+        let mut current_user_data = user_data;
+
+        let mut get_data = async move |current_data: &mut AutofillData| -> bool {
+            rx
+                .wait_for(|data| *data != *current_data)
+                .await
+                .map(|ref_data| {
+                    *current_data = (*ref_data).clone();
+                })
+                .is_ok()
+        };
+
+        eprintln!("waiting for data store request!");
+
+        while get_data(&mut current_user_data).await {
+            eprintln!("updated!!!");
+            let json = serde_json::to_string(&current_user_data).unwrap();
+            tauri::async_runtime::spawn_blocking(move || {
+                std::fs::write(USER_DATA_PATH, json)
+            }).await.unwrap().unwrap();
+        }
     });
+}
+
+
+#[cfg_attr(mobile, tauri::mobile_entry_point)]
+pub fn run() {
+
+    let (send_channel, rcv_channel) =
+        oneshot::channel::<(watch::Sender<AutofillData>, watch::Receiver<AutofillData>)>();
+    let (started_init, wait_for_start) = oneshot::channel();
+
+    tauri::async_runtime::spawn(USER_DATA_CHANNEL.get_or_init(async move || {
+        started_init.send(()).unwrap();
+        rcv_channel.await.expect("failed to initialize user data DB")
+    }));
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            fetch_request_info,
+            confirm_request,
+            load_data_from_store,
+            store_data_to_store
+        ])
+        .setup(|app| {
+            static ONCE: Once = Once::new();
+            ONCE.call_once(|| setup_once(app, wait_for_start, send_channel));
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { invoke } from "@tauri-apps/api/core";
 
-type RequestedData = {
+type RequestedDataMask = {
   name: boolean;
   email: boolean;
   phone_number: boolean;
@@ -10,14 +10,212 @@ type RequestedData = {
   id_image: boolean;
 }
 
-async function fetch_request_info(code: string): Promise<RequestedData> {
-  return (await invoke("requested_data", { code })) as RequestedData;
+type RequestedData = {
+  name: string[];
+  email: string;
+  phone_number: string;
+  id: string;
+  profile_picture: string;
+  license: string;
+  id_image: string;
+}
+
+async function fetch_request_info(code: string): Promise<RequestedDataMask> {
+  return (await invoke("fetch_request_info", { code })) as RequestedDataMask;
+}
+
+async function load_data_from_store(): Promise<RequestedData> {
+  return (await invoke("load_data_from_store", {})) as RequestedData;
+}
+
+async function store_data_to_store(data: RequestedData): Promise<void> {
+  await invoke("store_data_to_store", { data })
 }
 
 
-async function confirm_request(code: string): Promise<void> {
-  (await invoke("confirm_request", { code }))
+async function confirm_request(code: string, filter: RequestedDataMask): Promise<void> {
+  (await invoke("confirm_request", { code, filter }))
 }
 
 // @ts-ignore
 let _UNUSED = [fetch_request_info, confirm_request];
+
+
+let inside = false;
+function get(id: string) {
+  let autofill = document.getElementById(id);
+  if (autofill == null)
+  {
+    throw new Error("oops");
+    exit_dialog();
+  }
+  return autofill;
+}
+
+function getq(query: string) {
+  let autofill = document.querySelector(query);
+  if (autofill == null)
+  {
+    throw new Error("oops");
+    exit_dialog();
+  }
+  return autofill;
+}
+
+function getImage(element: HTMLInputElement): Promise<string> {
+  return new Promise((res, rej) => {
+    if (element.files == null || element.files[0] == null) {
+      res("data:image/png;base64,");
+      return;
+    }
+    let file = element.files[0];
+    let r = new FileReader();
+    r.addEventListener("load", () => {
+      res(r.result as string);
+    });
+    r.addEventListener("error", e => {
+      rej(e);
+    });
+    r.readAsDataURL(file);
+  });
+}
+
+function exit_dialog() {
+  const autofill = get("entercode");
+  const confirmer = get("confirm");
+  const devmode = get("devmode");
+  autofill.style.display = "none";
+  confirmer.style.display = "none";
+  devmode.style.display = "none";
+  inside = true;
+  setTimeout(() => {
+    inside = false;
+  }, 500);
+}
+function main() {
+  const autofill = get("entercode");
+  const codein = get("codein") as HTMLInputElement;
+  const confirmer = get("confirm");
+  const accesser = get("accesser");
+  const devmode = get("devmode");
+  const nodevmode = get("nodevmode");
+  const canceller = get("canceller");
+  let key = "";
+
+  canceller.addEventListener("click", () => {
+    exit_dialog();
+  });
+  document.body.addEventListener("click", async e => {
+    let bottomness= e.y > window.innerHeight * 2/3;
+    if (inside)
+      return;
+    if (bottomness) {
+      let info = await load_data_from_store();
+      devmode.style.display = "block";
+      let name = info.name[0] + " " + info.name[1];
+      (getq("input[data-absher=name]") as HTMLInputElement).value = name;
+      (getq("input[data-absher=email]") as HTMLInputElement).value = info.email;
+      (getq("input[data-absher=phone_number]") as HTMLInputElement).value = info.phone_number;
+      (getq("input[data-absher=id]") as HTMLInputElement).value = info.id;
+    } else {
+      autofill.style.display = "block";
+    }
+    inside = true;
+  });
+  codein.addEventListener("input", async () => {
+    key = codein.value;
+    key = key.toUpperCase();
+    key = key.replace(/[^A-Z]/g, "");
+    codein.value = key;
+    if (key.length == 9) {
+      autofill.style.display = "none";
+      let data;
+      try {
+        data = await fetch_request_info(key);
+      } catch (_) {
+        exit_dialog();
+        return;
+      }
+      confirmer.style.display = "block";
+      for (const [key, val] of Object.entries(data)) {
+        let element = document.querySelector(`tr[data-absher="${key}"]`);
+        if (element == null)
+        {
+          exit_dialog();
+          return;
+        }
+        (element as HTMLTableRowElement).style.display = val ? "block" : "none";
+        let element2 = document.querySelector(`tr[data-absher="${key}"] input`);
+        if (element2 == null)
+        {
+          exit_dialog();
+          return;
+        }
+        (element as HTMLInputElement).checked = true;
+      }
+    }
+  });
+
+  accesser.addEventListener("click", async e => {
+    exit_dialog();
+    e.preventDefault();
+    let dict: RequestedDataMask = {
+      name: false,
+      email: false,
+      phone_number: false,
+      id: false,
+      profile_picture: false,
+      license: false,
+      id_image: false,
+    };
+    for (const key of Object.keys(dict)) {
+      let element = document.querySelector(`tr[data-absher="${key}"] input`);
+      if (element == null)
+      {
+        exit_dialog();
+        return;
+      }
+      // @ts-ignore
+      dict[key] = (element as HTMLInputElement).checked;
+    }
+
+    await confirm_request(key, dict);
+    return 0;
+  });
+
+  nodevmode.addEventListener("click", async () => {
+    exit_dialog();
+    try {
+      let names = (getq("input[data-absher=name]") as HTMLInputElement).value.split(" ");
+
+      let [profile_picture, license, id_image] = await Promise.all([
+        getImage(getq("input[data-absher=profile_picture]") as HTMLInputElement),
+        getImage(getq("input[data-absher=license]") as HTMLInputElement),
+        getImage(getq("input[data-absher=id_image]") as HTMLInputElement),
+      ]);
+
+      let first_name = names[0] ?? "";
+      let last_name = names.slice(1).join(' ');
+      let info: RequestedData = {
+        name: [first_name, last_name],
+        email: (getq("input[data-absher=email]") as HTMLInputElement).value,
+        phone_number: (getq("input[data-absher=phone_number]") as HTMLInputElement).value,
+        id: (getq("input[data-absher=id]") as HTMLInputElement).value,
+        profile_picture,
+        license,
+        id_image
+      };
+
+// @ts-ignore
+      let what = info;
+
+      await store_data_to_store(info);
+    } catch (e) {
+      document.body.innerHTML = "";
+      document.body.innerText = (e as any).toString();
+      return;
+    }
+  });
+}
+
+main();
